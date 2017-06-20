4a283231e9e349a59dabf0e1ff07753b85b147ca
James Montemagno <james.montemagno@gmail.com>
2017-06-20 10:09:51 -0700

    Update sample app

--- samples/SettingsSample/SettingsSample/Helpers/Settings.cs
+++ samples/SettingsSample/SettingsSample/Helpers/Settings.cs
@@ -1,43 +1,29 @@
-// Helpers/Settings.cs
-using Refractored.Xam.Settings;
-using Refractored.Xam.Settings.Abstractions;
+﻿using Plugin.Settings;
+using Plugin.Settings.Abstractions;
 
 namespace SettingsSample.Helpers
 {
-  /// <summary>
-  /// This is the Settings static class that can be used in your Core solution or in any
-  /// of your client applications. All settings are laid out the same exact way with getters
-  /// and setters. 
-  /// </summary>
-  public static class Settings
-  {
-    private static ISettings AppSettings
+    public static class Settings
     {
-      get
-      {
-        return CrossSettings.Current;
-      }
+        private static ISettings AppSettings =>
+            CrossSettings.Current;
+
+
+        #region Setting Constants
+
+        private const string SettingsKey = "settings_key";
+        private static readonly string SettingsDefault = string.Empty;
+
+        #endregion
+
+
+        public static string GeneralSettings
+        {
+            get => AppSettings.GetValueOrDefault(SettingsKey, SettingsDefault);
+
+            set => AppSettings.AddOrUpdateValue(SettingsKey, value);
+
+        }
+
     }
-
-    #region Setting Constants
-
-    private const string SettingsKey = "settings_key";
-    private static readonly string SettingsDefault = string.Empty;
-
-    #endregion
-
-
-    public static string GeneralSettings
-    {
-      get
-      {
-        return AppSettings.GetValueOrDefault(SettingsKey, SettingsDefault);
-      }
-      set
-      {
-        AppSettings.AddOrUpdateValue(SettingsKey, value);
-      }
-    }
-
-  }
 }
