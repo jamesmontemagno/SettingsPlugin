81f4d63085822d9001fb4890a49eb90bc1405d05
Prashant Cholachagudda <pvc@outlook.com>
2018-01-30 22:03:23 +0530

    Fixed the Android unit test failures (#114)
    * Setup iOS unit tests
    
    - Added the runtests.sh
    - Included the Touch.Server.exe
    - Set xml test result generation evn variables
    
    * Update unit tests
    
    - Renamed tests to be more descriptive
    - Removed redundant tests
    
    * added the android unit test tasks
    
    * Added the test instrumention class
    
    * update the android sdk path
    
    * Added the Droid unit test target to cake build
    
    * added the make file to run iOS unit tests
    
    * added the contribution guide
    
    * added the checklist
    
    * Remove EXEs from the repo, instead download on demand
    
    * Simplified Android unit tests setup
    
    * Attempt to fix unit tests
    
    * Attempt to fix unit tests
    
    * Updated the Cross settings
    
    * removed portable projct
    
    * Reverting back to commit-cb2fe5e
    
    Fixed the review findings for PR #114

--- src/Plugin.Settings.Shared/CrossSettings.cs
+++ src/Plugin.Settings.Shared/CrossSettings.cs
@@ -20,15 +20,15 @@ namespace Plugin.Settings
     {
         static Lazy<ISettings> implementation = new Lazy<ISettings>(() => CreateSettings(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
 
-		/// <summary>
-		/// Gets if the plugin is supported on the current platform.
-		/// </summary>
-		public static bool IsSupported => implementation.Value == null ? false : true;
+        /// <summary>
+        /// Gets if the plugin is supported on the current platform.
+        /// </summary>
+        public static bool IsSupported => implementation.Value == null ? false : true;
 
-		/// <summary>
-		/// Current plugin implementation to use
-		/// </summary>
-		public static ISettings Current
+        /// <summary>
+        /// Current plugin implementation to use
+        /// </summary>
+        public static ISettings Current
         {
             get
             {
@@ -47,13 +47,13 @@ namespace Plugin.Settings
             return null;
 #else
 #pragma warning disable IDE0022 // Use expression body for methods
-			return new SettingsImplementation();
+            return new SettingsImplementation();
 #pragma warning restore IDE0022 // Use expression body for methods
 #endif
-		}
+        }
 
         internal static Exception NotImplementedInReferenceAssembly() =>
-			new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
-        
+            new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+
     }
 }
